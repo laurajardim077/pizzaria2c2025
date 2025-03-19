b6ff4d8c1db2d84b98eb342823d9654f7266b827
laurajardim007 <rm94797@estudante.fieb.edu.br>
2025-03-19 08:27:52 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
 function App() {
   const [nome, setNome] = useState('Ronaldo')
-  return (
+
+  const Formulario = () => {
+
     <div>
-      <h3>PIZZARIA 2C</h3>
       <input
         className="nome"
-        onChange={(e)=>{setNome(e.target.value)}}
+        onChange={(e) => { setNome(e.target.value) }}
         name="nome"
         type="text" />
       <button
@@ -15,8 +16,32 @@ function App() {
         CLIQUE AQUI
       </button>
     </div>
-  )
+  }
+  return (
+    <div>
+      <h3>PIZZARIA 2C</h3>
+      <input
+        className="nome"
+        onChange={(e) => { setNome(e.target.value) }}
+        name="nome"
+        type="text" />
+      <button
+        onClick={() => { alert(nome) }}
+        classname='botao'>
+        CLIQUE AQUI
+      </button>
+    </div>
+  );
 
-}
+};
+
+return (
+  <div>
+    <h3> PIZZARIA 2C</h3>
+    <Formulario/>
+    <Formulario/>
+    <Formulario/>
+  </div>
+);
 
 export default App 
