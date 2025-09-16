e2aaf172997fd8d40245bcc767a5827beece1721
laurajardim077 <rm94797@estudante.fieb.edu.br>
2025-09-16 07:41:36 -0300

    componente

---
+++ src/pages/produto/index.jsx
@@ -0,0 +1,17 @@
+const Produtos = () => {
+ 
+ 
+ return(
+    <div>
+        <h3>Listagem de Produtos</h3>
+        <ul>
+            <li>Pizza de Calabresa</li>
+            <li>Pizza de Muçarela</li>
+            <li>Pizza Dois Queijo</li>
+        </ul>
+ 
+    </div>
+ );
+}
+export default Produtos
+ 
